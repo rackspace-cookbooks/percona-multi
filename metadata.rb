2cdd4b2ddcf843d1b6d76e8a97e4de27619cdd49
Matthew Thode <mthode@mthode.org>
2015-03-16 11:19:35 -0500

    bumping for dev

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Rackspace Hosting, Inc.'
 maintainer_email 'rackspace-cookbooks@rackspace.com'
 license          'Apache 2.0'
 description      'Provides Percona master/slave replication services'
-version          '0.1.2'
+version          '0.1.3'
 
 depends 'build-essential'
 depends 'percona', '< 0.16'
